62128f9511efa71cdd5a77a19f4e8e7ece105f2d
Natalie Jann <nataliejann@live.com>
2020-08-06 16:10:25 +0200

    ENH: add errorhandlers to TL demo

--- rcnn-implementations/rcnn-pytorch/tl-demo/app.py
+++ rcnn-implementations/rcnn-pytorch/tl-demo/app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, url_for, redirect
-from werkzeug.utils import secure_filename
-import requests
 import os
+
 import numpy as np
+import requests
+from werkzeug.utils import secure_filename
+
+from flask import Flask, abort, redirect, render_template, request, url_for
 
 
 BENTO_URL = 'https://bentoml-her0ku-mtu5njy5odi3mgo.herokuapp.com'
-
 
 app = Flask(__name__)
 
@@ -18,22 +19,19 @@ def say_hello():
 
 @app.route('/analyze', methods=['POST'])
 def classify():
-    error = False
     image = request.files['unpredicted']
-    name, ext = os.path.splitext(secure_filename(image.filename))
-    # if ext.lower() not in ('.png', '.jpg', '.jpeg'):
-    #     # let's have some fun
-    #     return HTTPResponse(status=418)
     filename = secure_filename(image.filename)
+    name, ext = os.path.splitext(filename)
+    if ext.lower() not in ('.png', '.jpg', '.jpeg'):
+        # let's have some fun
+        abort(418)
     image.save(os.path.join('static', 'images', filename))
-    # files = {'media': open('test.jpg', 'rb')}
-    # requests.post(url, files=files)
     with open(os.path.join('static', 'images', filename), 'rb') as input:
         rqt = requests.post(BENTO_URL + '/predict', headers={'Content-Type': 'image/*'},
                             data=input.read())
     if rqt.status_code != 200:
-        error = True
-    return render_template('result.html', error=error, pred=rqt.text, filename=url_for('static', filename='images/' + filename))
+        abort(500)
+    return render_template('result.html', pred=rqt.text, filename=url_for('static', filename='images/' + filename))
 
 
 @app.route('/del/<img>', methods=['GET'])
@@ -44,5 +42,20 @@ def delete_image(img):
     return redirect(url_for('say_hello'))
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html', error=404), 404
+
+
+@app.errorhandler(418)
+def page_not_found(error):
+    return render_template('error.html', error=418), 418
+
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template('error.html', error=500), 500
+
+
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(port=5000)
--- rcnn-implementations/rcnn-pytorch/tl-demo/setup.py
+++ rcnn-implementations/rcnn-pytorch/tl-demo/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
 
+
 setup(
     name='tl-demo',
     version='1.0.0',
