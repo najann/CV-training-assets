38db4a352672598e8b461bb32fc630d5ec8e73aa
Natalie Jann <nataliejann@live.com>
2020-08-17 15:49:27 +0200

    ENH: enable image upload and class prediction for TFJS demo,

--- rcnn-implementations/rcnn-keras/tl_tfjs_demo/index.js
+++ rcnn-implementations/rcnn-keras/tl_tfjs_demo/index.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var app = express();
 const path = require("path");
+
 const router = express.Router();
+
+app.use(express.static("static"));
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/home.html"));
 });
 
 app.use("/", router);
-app.use(express.static("static"));
+
 app.listen(process.env.port || 8081);
--- rcnn-implementations/rcnn-keras/tl_tfjs_demo/static/js/model.js
+++ rcnn-implementations/rcnn-keras/tl_tfjs_demo/static/js/model.js
@@ -1,28 +1,23 @@
 async function init() {
-    model = await tf.loadLayersModel("http://127.0.0.1:8000/");
+  model = await tf.loadLayersModel("http://127.0.0.1/");
+}
 
-    const imgEl = document.getElementById("img");
-    const offset = tf.scalar(127.5);
-    let input = tf.browser.fromPixels(imgEl);
+async function analyze() {
+  const imgEl = document.getElementById("output");
+  let input = tf.browser.fromPixels(imgEl);
 
-    input = tf.tidy(() => {
-        const a = input
-            .resizeNearestNeighbor([224, 224])
-            .toFloat()
-            .sub(offset)
-            .div(offset)
-            .expandDims();
-        // .reshape([1, 224, 224, 3])
-        // .toFloat()
-        // .div(tf.scalar(127))
-        // .sub(tf.scalar(1));
-        return a;
-    });
+  input = tf.tidy(() => {
+    const a = input
+      .resizeNearestNeighbor([224, 224])
+      .toFloat()
+      .expandDims();
+    return a;
+  });
 
-    let result = await model.predict(input).array();
-    result = result[0][0] < 0.5 ? 0 : 1;
-    input.dispose()
-    console.log(result)
+  let result = await model.predict(input).array();
+  result = result[0][0] < 0.5 ? "cat" : "dog";
+  input.dispose();
+  alert(result);
 }
 
 init();
