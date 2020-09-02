1332973e6d95a9621a9efdc2d7405f7be45b1bbc
Natalie Jann <nataliejann@live.com>
2020-09-02 21:25:42 +0200

    ENH: make tfjs demo work with docker compose

--- rcnn-implementations/rcnn-keras/tl_tfjs_demo/fastapi_model/main.py
+++ rcnn-implementations/rcnn-keras/tl_tfjs_demo/fastapi_model/main.py
@@ -8,7 +8,20 @@ app = FastAPI()
 # configure CORSMiddleware so that the
 # frontend can request the model files
 origins = [
-    "http://127.0.0.1:8081/",
+    # "http://0.0.0.0:8080/",
+    # "http://0.0.0.0:8080",
+    # "http://localhost:8080/",
+    # "http://localhost:8080",
+    # "http://127.0.0.1:8080/",
+    # "http://127.0.0.1:8080",
+    # "https://0.0.0.0:8080/",
+    # "https://0.0.0.0:8080",
+    # "https://localhost:8080",
+    # "https://localhost:8080/",
+    # "https://127.0.0.1:8080",
+    # "https://127.0.0.1:8080/",
+    # "http://api/",
+    # "https://api/",
     "*"
 ]
 
--- rcnn-implementations/rcnn-keras/tl_tfjs_demo/static/js/model.js
+++ rcnn-implementations/rcnn-keras/tl_tfjs_demo/static/js/model.js
@@ -1,5 +1,7 @@
+var model;
 async function init() {
-  model = await tf.loadLayersModel("api:80");
+  model = await tf.loadLayersModel("http://localhost:80/");
+  console.log("ready");
 }
 
 async function analyze() {
@@ -13,7 +15,7 @@ async function analyze() {
       .expandDims();
     return a;
   });
-
+  console.log(model);
   let result = await model.predict(input).array();
   result = result[0][0] < 0.5 ? "cat" : "dog";
   input.dispose();
