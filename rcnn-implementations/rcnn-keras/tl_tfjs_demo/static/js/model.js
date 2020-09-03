a9d27ff2cf6739c3e33cedbd7a529d8f586de061
Natalie Jann <nataliejann@live.com>
2020-09-03 20:28:51 +0200

    FEAT: add ibm cloud k8s deployment files

--- rcnn-implementations/rcnn-keras/tl_tfjs_demo/static/js/model.js
+++ rcnn-implementations/rcnn-keras/tl_tfjs_demo/static/js/model.js
@@ -1,25 +1,51 @@
-var model;
 async function init() {
-  model = await tf.loadLayersModel("http://localhost:80/");
-  console.log("ready");
+  document.getElementById("analysis-result-loader").style.display = 'none';
+  document.getElementById("model-loader").style.display = 'grid';
+  document.getElementById("main").style.display = 'none';
+  document.getElementById("backbutton").style.display = 'none';
+  document.getElementById("submit").disabled = true;
+
+  try {
+    model = await tf.loadLayersModel("http://localhost:8081");
+  } catch (error) {
+    try {
+      model = await tf.loadLayersModel("http://localhost:80");
+    } catch (samerror) {
+      try {
+        model = await tf.loadLayersModel("http://173.193.68.8:30002/");
+      } catch (err) {
+        document.getElementById("model-loader").style.display = 'none';
+        alert('For some unknown reason, the model could not be retrieved. Please try again in a different browser.')
+      }
+    }
+  }
+
+  document.getElementById("model-loader").style.display = 'none';
+  document.getElementById("main").style.display = 'grid';
 }
 
-async function analyze() {
-  const imgEl = document.getElementById("output");
-  let input = tf.browser.fromPixels(imgEl);
+async function analyze(inp) {
+  // document.getElementById("analysis-result-loader").style.display = 'grid';
+
+
+  // document.getElementById("main").style.display = 'none';
 
   input = tf.tidy(() => {
-    const a = input
+    const a = inp
       .resizeNearestNeighbor([224, 224])
       .toFloat()
       .expandDims();
     return a;
   });
-  console.log(model);
   let result = await model.predict(input).array();
   result = result[0][0] < 0.5 ? "cat" : "dog";
   input.dispose();
-  alert(result);
+
+  document.getElementById("analysis-result-loader").style.display = 'none';
+  document.getElementById("result").innerHTML = '<h4>This is most likely a ' + result + '</h4><canvas id="resultimage"></canvas>';
+  tf.browser.toPixels(inp, document.getElementById('resultimage'));
+  document.getElementById("backbutton").style.display = 'grid';
+
 }
 
 init();
