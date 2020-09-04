2bee8f505392dc24e525e4da688fdb6a0c64efbc
Natalie Jann <nataliejann@live.com>
2020-09-04 10:30:06 +0200

    ENH: add cache control to fastapi app

--- rcnn-implementations/rcnn-keras/tl_tfjs_demo/fastapi_model/main.py
+++ rcnn-implementations/rcnn-keras/tl_tfjs_demo/fastapi_model/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import re
+
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 app = FastAPI()
@@ -33,15 +34,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+oneMonth = "max-age=2592000"
 
 # initial request
 @app.get("/")
-async def root():
+async def root(response: Response):
+    response.headers["Cache-Control"] = oneMonth
     return FileResponse('model-tfjs-layer/model.json')
 
 # subsequent (automatic) requests
 @app.get("/{shard}")
-async def sub_req(shard):
+async def sub_req(shard, response: Response):
+    response.headers["Cache-Control"] = oneMonth
     regex = r'^group1-shard(\d{1,2})of(\d{1,2}\.)bin$'
     if re.match(regex, shard):
         return FileResponse(f'model-tfjs-layer/{shard}')
